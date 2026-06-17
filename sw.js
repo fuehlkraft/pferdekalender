6728ad7ebcc2ee4fc95939d514d827b8430e662a
fuehlkraft <morgensternland@googlemail.com>
2026-06-17 21:58:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Fühlkraft Kalender - Service Worker
-const CACHE_NAME = 'fuehlkraft-kalender-v5';
+const CACHE_NAME = 'fuehlkraft-kalender-v6';
 
 // Installation
 self.addEventListener('install', function(e){
